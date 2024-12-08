e0a7609227e172ae78ee409cf2a134945a41cc61
Daniel Servejeira <servejeiradaniel@gmail.com>
2024-12-08 14:12:53 -0300

    fix: multi language supported main

--- main.py
+++ main.py
@@ -1,85 +1,65 @@
 import os
-import importlib
+from translates.main_translation import translate
+from algorithms.fractional_knapsack_problem import run_fractional_knapsack_interface
+from algorithms.boolean_knapsack_problem import run_boolean_knapsack_interface
+from algorithms.lcs import run_lcs_interface
 
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-def program_functionality(language: int) -> int:
-    program_functionality = 0
+def problem(language: str) -> int:
+    problem = -1
 
-    if language == 1:
-        while program_functionality not in [1, 2, 3, 4, 5]:
-            print("""
-                    By Daniel Servejeira & Pedro Alonso Oliveira\n
-                    Choose a problem:
-                    [1] <- Assignment Problem
-                    [2] <- Huffman Coding
-                    [3] <- Fractional Knapsack Problem
-                    [4] <- Boolean Knapsack Problem
-                    [5] <- Longest Common Subsequence\n
-                """)
-            try:
-                program_functionality = int(input("Enter your choice: "))
-            except ValueError:  
-                print("Please enter a valid number.\n")
-                clear_screen()
+    while problem not in [0, 1, 2, 3, 4, 5]:
+        print(f"""
+            {translate(language, 'author')}\n
+                {translate(language, 'problem_choosing')}
+                [1] <- {translate(language, 'assignment_problem')}
+                [2] <- {translate(language, 'huffman_coding')}
+                [3] <- {translate(language, 'fractional_knapsack_problem')}
+                [4] <- {translate(language, 'boolean_knapsack_problem')}
+                [5] <- {translate(language, 'longest_common_subsequence')}
+                [0] <- {translate(language, 'exit')}\n
+            """)
+        try:
+            problem = int(input(f"{translate(language, 'program_functionality')} "))
+        except ValueError:  
+            print(f"{translate(language, "value_error")}\n")
+            clear_screen()
 
-    elif language == 2:
-        while program_functionality not in [1, 2, 3, 4, 5]:
-            print("""
-                    Por Daniel Servejeira & Pedro Alonso Oliveira\n
-                    Escolha um problema:
-                    [1] <- Problema de Associação de Tarefas
-                    [2] <- Codificação de Huffman
-                    [3] <- Problema da Mochila Fracionária
-                    [4] <- Problema da Mochila Booleana
-                    [5] <- Problema da Subsequência Comum Máxima\n
-                """)
-            try:
-                program_functionality = int(input("Digite sua escolha: "))
-            except ValueError:  
-                print("Por favor digite um número válido.\n")
+    return problem
 
-    return program_functionality
-
-program_functionality = 0
-language = 0
+opt = -1
 supported_languages = 2
+program_functionality = -1
 
-functionality_map = {
-    1: "assignment_problem",
-    2: "huffman_coding",
-    3: "fractional_knapsack_problem",
-    4: "boolean_knapsack_problem",
-    5: "lcs"
-}
-
-while language < 1 or language > supported_languages:
+while opt < 0 or opt > supported_languages:
     print("""
-            Select your language: 
-            [1] <- English (Coming soon)
-            [2] <- Portuguese\n
+            Select your language:\n
+                [0] <- EN-US
+                [1] <- PT-BR\n
           """)
     try:
-        language = int(input("Enter your choice: "))
+        opt = int(input("Enter your choice: "))
     except ValueError:
         print("Please enter a valid number.\n")
         clear_screen()
 
-if language == 1:
-    interface_path = english_interfaces_path
-elif language == 2:
-    interface_path = portuguese_interfaces_path
+language = {0: "en-us",
+            1: "pt-br"}
 
-program_functionality = program_functionality(language)
+while program_functionality != 0:
+    program_functionality = problem(language[opt])
 
-module_name = functionality_map.get(program_functionality)
-
-if module_name:
-    try:
-        # Dynamically import the module and run the function
-        module = importlib.import_module(f"{interface_path}.{module_name}")
-        run_function = getattr(module, f"run_{module_name}_interface")
-        run_function()  # Call the function
-    except (ImportError, AttributeError) as e:
-        print(f"Error: {e}")
+    if(program_functionality == 0):
+        break
+    elif(program_functionality == 1):
+        run_assignment_problem_interface(language[opt])
+    elif(program_functionality == 2):
+        run_huffman_coding_interface(language[opt])
+    elif(program_functionality == 3):
+        run_fractional_knapsack_interface(language[opt])
+    elif(program_functionality == 4):
+        run_boolean_knapsack_interface(language[opt])
+    elif(program_functionality == 5):
+        run_lcs_interface(language[opt])
